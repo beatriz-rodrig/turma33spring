04a6c9c33a79a01a1b012e3556c43d16cc79c6e0
beatriz-rodrig <beatriz_rodrigues1993@hotmail.com>
2021-09-30 16:13:46 -0300

    aula

---
+++ src/main/java/com/gen/gamesGeneration/model/Categoria.java
@@ -0,0 +1,85 @@
+package com.gen.gamesGeneration.model;
+
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@Entity
+@Table(name = "tb_categoria")
+
+public class Categoria {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	
+	@NotNull
+	@Size(min = 5, max = 100)
+	private String titulo; //nome do jogo
+	
+	@NotNull
+	@Size(min = 5, max = 200)
+	private String descrição; //descrição
+
+	@NotNull
+	private double valor; //valor
+	
+
+	@ManyToOne
+	@JoinColumn(name = "produto")
+	private Produto produto;
+	
+
+	public Produto getProduto() {
+		return produto;
+	}
+
+	public void setProduto(Produto produto) {
+		this.produto = produto;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	public String getDescrição() {
+		return descrição;
+	}
+
+	public void setDescrição(String descrição) {
+		this.descrição = descrição;
+	}
+
+	public double getValor() {
+		return valor;
+	}
+
+	public void setValor(double valor) {
+		this.valor = valor;
+	}
+	
+	
+
+}
+	
---
+++ src/main/java/com/gen/gamesGeneration/repository/CategoriaRepository.java
@@ -0,0 +1,14 @@
+package com.gen.gamesGeneration.repository;
+
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.gen.gamesGeneration.model.Categoria;
+
+@Repository
+public interface CategoriaRepository extends JpaRepository<Categoria, Long> {
+	public List<Categoria> findAllByDescricaoContainingIgnoreCase(String descricao);
+}
